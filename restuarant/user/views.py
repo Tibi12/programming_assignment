2975409cf2cd9fea357526671957449f70d4b13e
Tibi12 <43844668+Tibi12@users.noreply.github.com>
2024-04-12 00:18:56 +0100

    fixing signup

--- restuarant/user/views.py
+++ restuarant/user/views.py
@@ -12,9 +12,12 @@ def login(request):
     if request.method == 'POST':
         form = CustomAuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('user:home')
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('user:home')
     else:
         form = CustomAuthenticationForm()
     return render(request, 'login.html', {'form': form})
@@ -23,8 +26,9 @@ def signup(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            login(request, user)
+            form.save()
+            user_name = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {user_name}.')
             return redirect('user:home')
     else:
         form = CustomUserCreationForm()
