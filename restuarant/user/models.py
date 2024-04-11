e280c78d78425bbeaea74045906e64808df70293
Tibi12 <43844668+Tibi12@users.noreply.github.com>
2024-04-11 22:59:37 +0100

    fix error

--- restuarant/user/models.py
+++ restuarant/user/models.py
@@ -5,11 +5,6 @@ class CustomUser(AbstractUser):
     email = models.EmailField(unique=True)
     # Add any additional fields you need
 
-    class Meta:
-        # Specify unique related names for the groups and user_permissions fields
-        # to avoid clashes with the built-in User model
-        permissions = (
-            ('group_permissions', 'Group permissions'),
-            ('user_permissions', 'User permissions'),
-        )
+    def __str__(self):
+        return self.username
 
