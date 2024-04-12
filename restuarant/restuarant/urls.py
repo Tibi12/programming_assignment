0659c381cf0d4af80e484587bff9ab4f64be59e3
Najeebvm <140195918+Najeebvm@users.noreply.github.com>
2024-04-12 19:33:18 +0530

    correcting url

--- restuarant/restuarant/urls.py
+++ restuarant/restuarant/urls.py
@@ -20,5 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/',include('user.urls')),
-    path('menuitems/',include('menuitems.urls'))
+    path('menuitems/',include('menuitems.urls')),
+    path('orderdashboard/',include('orderdashboard.urls'))
 ]
