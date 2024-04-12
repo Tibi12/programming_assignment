c56502f1f329b5550495de4a722b4657a1638d35
Najeebvm <140195918+Najeebvm@users.noreply.github.com>
2024-04-12 20:06:02 +0530

    corect the error

--- restuarant/orderdashboard/urls.py
+++ restuarant/orderdashboard/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('orderdashboard', views.items, name='orderdashboard'),
+    path('orderdashboard/', views.items, name='orderdashboard'),
 ]
-
