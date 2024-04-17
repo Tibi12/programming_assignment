4a725f2d3a7cc5296c43a90cff49283e19a67224
Tibi12 <43844668+Tibi12@users.noreply.github.com>
2024-04-17 11:49:55 +0000

    registered models to admin page

--- restaurant/kitchen_dashboard/admin.py
+++ restaurant/kitchen_dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Table, Order, TableCart
 
-# Register your models here.
+admin.site.register(Table)
+admin.site.register(TableCart)
+admin.site.register(Order)
