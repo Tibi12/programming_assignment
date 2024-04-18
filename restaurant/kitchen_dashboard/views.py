06a5f7be49899b1cf618a201c9874d6440f234a7
Najeebvm <140195918+Najeebvm@users.noreply.github.com>
2024-04-18 16:20:12 +0000

    api to deliver order

--- restaurant/kitchen_dashboard/views.py
+++ restaurant/kitchen_dashboard/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Order
 
 def kitchen_dashboard(request):
     orders = Order.objects.filter(order_type="active").order_by('table')  
@@ -6,11 +7,19 @@ def kitchen_dashboard(request):
     context = {'orders': orders}
         
     return render(request, 'dashboard.html', context)
-
+    
 def accept_order(request):
     order_id = request.POST.get('order_id')
     order = Order.objects.get(pk=order_id)
     order.status = "cooking"
+    order.save()
+    
+    return redirect('kitchen_dashboard')
+
+def deliver_order(request):
+    order_id = request.POST.get('order_id')
+    order = Order.objects.get(pk=order_id)
+    order.status = "completed"
     order.save()
     
     return redirect('kitchen_dashboard')
