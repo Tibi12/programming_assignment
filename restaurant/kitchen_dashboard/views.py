d13bc56a3967f896bed6338e933ea9121513471c
Najeebvm <140195918+Najeebvm@users.noreply.github.com>
2024-04-18 16:13:42 +0000

    fetch orders

--- restaurant/kitchen_dashboard/views.py
+++ restaurant/kitchen_dashboard/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
-def kitchen_dashboard(request):       
-    return render(request, 'dashboard.html')
+def kitchen_dashboard(request):
+    orders = Order.objects.filter(order_type="active").order_by('table')  
+        
+    context = {'orders': orders}
+        
+    return render(request, 'dashboard.html', context)
