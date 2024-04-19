025b999530e4a80dc0ca241d39251a1549b73fc2
Tibi12 <43844668+Tibi12@users.noreply.github.com>
2024-04-19 17:37:49 +0000

    cleanup

--- restaurant/user/forms.py
+++ restaurant/user/forms.py
@@ -2,11 +2,6 @@ from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from django.contrib.auth.models import User
 from django import forms
 
-# class CustomAuthenticationForm(AuthenticationForm):
-#     class Meta:
-#         model = CustomUser
-
-
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
