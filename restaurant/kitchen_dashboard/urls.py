d9b7d7d17eda56d1d93b3060fd93ac5b15a0551a
colejnr <54003223+colejnr@users.noreply.github.com>
2024-04-19 18:12:30 +0000

    vacate table api url

--- restaurant/kitchen_dashboard/urls.py
+++ restaurant/kitchen_dashboard/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.kitchen_dashboard, name='kitchen_dashboard'),
     path('accept-order', views.accept_order, name='accept_order'),
     path('deliver-order', views.deliver_order, name='deliver_order'),
+    path('vacate-table', views.vacate_table, name='vacate_table'),
 ]
