a16414132ccc0763b0dd83e0402c70088146800a
colejnr <54003223+colejnr@users.noreply.github.com>
2024-04-19 18:23:56 +0000

    printing tablecart

--- restaurant/menu/views.py
+++ restaurant/menu/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-from kitchen_dashboard.models import Table
+from kitchen_dashboard.models import Table, TableCart
 from .models import Menu
-def menu_cart(request):
-    return render(request, 'cart.html')
 
+def menu_cart(request):
+    chosen_table = request.GET.get('table_number')
+    table = Table.objects.get(table_number=chosen_table)
+    table_carts = TableCart.objects.filter(table=table)
+    print(table_carts)
+
+    return render(request, 'cart.html')
 
 def menu_home(request):
     tables = Table.objects.all()
