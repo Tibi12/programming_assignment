20f796c225c289729396d822136ed27cb09e0dd2
Tibi12 <43844668+Tibi12@users.noreply.github.com>
2024-04-19 17:37:25 +0000

    new logic for AuthenticationForm

--- restaurant/user/forms.py
+++ restaurant/user/forms.py
@@ -13,3 +13,8 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.CharField(max_length=254)
+    password = forms.CharField(widget=forms.PasswordInput)
+    
