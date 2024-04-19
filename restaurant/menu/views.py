08b4fd0eef0196e627bca8130dab41045d085465
Eldhowilson14 <161327297+Eldhowilson14@users.noreply.github.com>
2024-04-19 18:38:57 +0000

    show available tables on menu

--- restaurant/menu/views.py
+++ restaurant/menu/views.py
@@ -6,6 +6,10 @@ def menu_home(request):
     tables = Table.objects.all()
     print(tables)
     menus = Menu.objects.all()
+
+    already_selected = request.GET.get('table', None)
+    if already_selected is None:
+        already_selected = False
         
-    context = { 'tables': tables, 'menus': menus }
+    context = { 'tables': tables, 'menus': menus, 'already_selected': already_selected }
     return render(request, 'menu.html', context)
