3807596cbd606a3c62ce87e0cbf468d1fde82c25
colejnr <54003223+colejnr@users.noreply.github.com>
2024-04-20 11:01:25 +0000

    path defined

--- restaurant/kitchen_dashboard/urls.py
+++ restaurant/kitchen_dashboard/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('place-order/<int:table_number>', views.place_order, name='place_order'),
     path('', views.kitchen_dashboard, name='kitchen_dashboard'),
     path('accept-order', views.accept_order, name='accept_order'),
     path('deliver-order', views.deliver_order, name='deliver_order'),
