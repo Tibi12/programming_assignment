8259ff1fd6a31c8f5e793621a8f030485071e2ee
Eldhowilson14 <161327297+Eldhowilson14@users.noreply.github.com>
2024-04-21 01:41:51 +0000

    code refractor

--- restaurant/menu/views.py
+++ restaurant/menu/views.py
@@ -1,35 +1,33 @@
-from django.shortcuts import render
-from kitchen_dashboard.models import Table, TableCart
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from kitchen_dashboard.models import Table, TableCart, Order
 from .models import Menu
+
+def menu_home(request):
+    tables = Table.objects.all()
+    menus = Menu.objects.all()
+    
+    already_selected = request.GET.get('table', None)
+    if already_selected is None:
+        already_selected = False
+        
+    context = {'tables': tables, 'menus': menus, 'already_selected': already_selected }
+    return render(request, 'menu.html', context)
 
 def menu_cart(request):
     chosen_table = request.GET.get('table_number')
     table = Table.objects.get(table_number=chosen_table)
     table_carts = TableCart.objects.filter(table=table)
-    print(table_carts)
-
+    
     table_carts_with_total_price = [
         {'name': item.name, 'quantity': item.quantity, 'price': item.price, 'total_price': item.quantity * item.price}
         for item in table_carts
     ]
     
     total_price = sum(item['total_price'] for item in table_carts_with_total_price)
-
+    
     context = {'chosen_table': chosen_table, 'table_carts': table_carts_with_total_price, 'total_price': total_price}
-
     return render(request, 'cart.html', context)
-
-def menu_home(request):
-    tables = Table.objects.all()
-    print(tables)
-    menus = Menu.objects.all()
-
-    already_selected = request.GET.get('table', None)
-    if already_selected is None:
-        already_selected = False
-        
-    context = { 'tables': tables, 'menus': menus, 'already_selected': already_selected }
-    return render(request, 'menu.html', context)
 
 def menu_orders(request):
     chosen_table = request.GET.get('table_number')
