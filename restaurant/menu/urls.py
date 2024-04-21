8667ef87baa90fd291c6fef262ddaefd23c20332
Eldhowilson14 <161327297+Eldhowilson14@users.noreply.github.com>
2024-04-21 01:39:46 +0000

    add top menu cart logic

--- restaurant/menu/urls.py
+++ restaurant/menu/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.menu_home, name='menu_home'),
     path('cart', views.menu_cart, name='menu_cart'),
     path('menu-orders', views.menu_orders, name='menu_orders'),
+    path('menu-cart', views.add_to_menu_cart, name='add_to_menu_cart'),
 ]
--- restaurant/menu/views.py
+++ restaurant/menu/views.py
@@ -37,3 +37,31 @@ def menu_orders(request):
     orders = Order.objects.filter(table=table, order_type="active")
     context = {'chosen_table': chosen_table, 'orders': orders}
     return render(request, 'menu_orders.html', context)
+
+def add_to_menu_cart(request):
+    if request.method == 'POST':
+        menu_id = request.POST.get('menu_id')
+        table_number = request.POST.get('table_number')
+        request.session['selected_table'] = table_number
+        
+        menu = None
+        table = Table.objects.get(table_number=table_number)
+        try:
+            menu = Menu.objects.get(pk=menu_id)
+        except Menu.DoesNotExist:
+            return redirect('menu_home')
+        
+        try:
+            table_cart_item = TableCart.objects.get(table=table, menu_item=menu)
+            table_cart_item.quantity += 1
+        except TableCart.DoesNotExist:
+            table_cart_item = TableCart.objects.create(
+                table=table,
+                menu_item=menu,
+                name=menu.name,
+                quantity=1,
+                price=menu.price
+            )
+        table_cart_item.save()
+            
+        return redirect(reverse('menu_home') + '?table='+str(table_number))
